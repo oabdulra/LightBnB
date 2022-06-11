10c5fa102a2e68742ed539158dc91677ee8fa388
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-11 17:44:20 -0400

    added query to look for signle user info

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -9,9 +9,9 @@ VALUES ('Eva Stanley','sebastianguerra@ymail.com','$2a$10$FB/BOAVhpuLvpOREQVmvme
 ('Leroy Hart','jaycereynolds@inbox.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms,number_of_bedrooms,country,street,city,province,post_code)
-VALUES ('1','Speed lamp','description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 'V8K 2K8', true),
-('2','Blank Corner','description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 4853.99, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 'Q5S BM7', true),
-('3','Habit Mix','description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 153.99, 1, 2, 3, 'Canada', '8589 Bread Road', 'London', 'Ontario', 'N0L 1M8', true);
+VALUES ('1','Speed lamp','description', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 930.61, 6, 4, 8, 'Canada', '536 Namsub Highway', 'Sotboske', 'Quebec', 'V8K 2K8'),
+('2','Blank Corner','description', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 4853.99, 6, 6, 7, 'Canada', '651 Nami Road', 'Bohbatev', 'Alberta', 'Q5S BM7'),
+('3','Habit Mix','description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 153.99, 1, 2, 3, 'Canada', '8589 Bread Road', 'London', 'Ontario', 'N0L 1M8');
 
 INSERT INTO reservations (start_date,end_date,property_id,guest_id)
 VALUES ('2018-09-11','2018-09-26',2,3),
@@ -20,11 +20,11 @@ VALUES ('2018-09-11','2018-09-26',2,3),
 ('2014-10-21','2014-11-11',3,5);
 
 INSERT INTO property_reviews (guest_id,property_id,reservation_id,rating,message)
-VALUES (2,5,10,3,'messages'),
-(1,4,1,4,'messages'),
-(8,1,2,4,'messages'),
-(3,8,5,4,'messages'),
-(4,2,7,5,'messages');
+VALUES (2,1,10,3,'messages'),
+(1,3,1,4,'messages'),
+(8,3,2,4,'messages'),
+(3,2,5,4,'messages'),
+(4,1,7,5,'messages');
 
 
 
