08e84b09df0a39af7c5e664dd7894b569d1802ea
Osama Abdul Rahmane <oabdul65@gmail.comm>
2022-06-15 12:06:07 -0400

    installed postgres as a dependancy

--- server/database.js
+++ server/database.js
@@ -1,7 +1,17 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
 
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
 /// Users
+
 
 /**
  * Get a single user from the database given their email.
